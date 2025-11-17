f3e2037765e9252eb47ef0855e68650d075791ce
harinsd404 <qlalfql123@gmail.com>
2025-11-17 11:40:01 +0000

    지금 왜 내컷밋이 아니오

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/mydb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:q1w2e3@localhost:3306/reach_you'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)  # 마이그레이션 연동
+migrate = Migrate(app, db)
 
-# 모델 예시
 class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
